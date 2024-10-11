2d39465c8105ded8ec98022560073ed7979921ea
mdabutalebdev <mdabutaleb.dev@gmail.com>
2024-10-11 23:32:11 +0600

    finsweet-tailwind css

---
+++ js/tailwind.config.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // color part start
+        colors: {
+            HeaderBg:"#1C1E53",
+            Header:"rgba(0, 0, 0, 0.6);",
+            textColor:"#BBBBCB",
+            BorderColor:"#F4F6FC",
+            ButtonBgColor:"#FCD980",
+            h3Color:"#282938",
+            clintColor:"#2405F2",
+            workBg:"#F4F6FC",
+            inquryOverlayColor:"rgba(28, 30, 83, 0.5"
+
+        },
+        // color part end
+
+        // container part start
+        width: {
+           headerContainer:"1280px",
+           footerContainer:"1091px",
+        },
+        // container part end
+        // font family start here
+         fontfamily: {
+          Poppins:"'Poppins', sans-serif",
+            
+        },
+          // font family end here
+        },
+      },
+    };
+ 
